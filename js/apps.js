20c68806888659109fd0a297d9c5511b306f182b
Fernando Mociulsky <fmociulsky@garbarino.com.ar>
2020-04-16 17:15:45 -0300

    avances con bootstrap

--- js/apps.js
+++ js/apps.js
@@ -1,3 +1,27 @@
-function mostrarOpciones(e){
-    console.log('hola');
+const btnDay = document.getElementById('btn-day');
+const btnNight = document.getElementById('btn-night');
+
+
+agregarEventListeners();
+
+function agregarEventListeners(){
+    //btnDay.addEventListener('click', aplicarTemaDay);
+    //btnNight.addEventListener('click', aplicarTemaNight);
+}
+
+
+function aplicarTemaDay(){
+    console.log('day');
+}
+
+function aplicarTemaNight(){
+    console.log('night');
+}
+
+function mostrarOpciones(){
+    const body = document.body;
+    body.classList.remove('body_light');
+    body.classList.add('body_night');
+    /*const divOpciones = document.getElementsByClassName('opciones')[0];
+    divOpciones.style.display = "block";*/
 }
