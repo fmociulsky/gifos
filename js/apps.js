682e6356a3f038ce2a6a35a99fda78af60cbbf67
Fernando Mociulsky <fmociulsky@garbarino.com.ar>
2020-04-17 17:44:22 -0300

    Avances con tema night

--- js/apps.js
+++ js/apps.js
@@ -1,14 +1,55 @@
 const btnDay = document.getElementById('btn-day');
 const btnNight = document.getElementById('btn-night');
+const apiKey = 'ueK2jQMCymNZn085I319vMXEtEjvNFGH';
 
 
 agregarEventListeners();
 
 function agregarEventListeners(){
+    document.addEventListener('DOMContentLoaded', onLoad());
     //btnDay.addEventListener('click', aplicarTemaDay);
     //btnNight.addEventListener('click', aplicarTemaNight);
 }
 
+function onLoad(){
+    console.log('Carga de pantalla');
+    cargarSugerencias();
+}
+
+function cargarSugerencias() {
+    const search = 'dog';
+    fetch('http://api.giphy.com/v1/gifs/search?q=' + search + '&api_key=' + apiKey)
+        .then(response => {
+            return response.json();
+        })
+        .then(resultado => {
+            const divSugerencias = document.getElementById('sugerencias');
+            for (let index = 0; index < 4; index++) {
+                const sugerencia = document.createElement('div');
+                const resImagen = resultado.data[index];
+                sugerencia.innerHTML = `<div class=" p-1 card-small"> 
+                                        <div class="header p-1">
+                                            <div class="d-flex justify-content-between">
+                                                <a href="#">#${resImagen.title}</a>
+                                                <img class="close align-self-center" src="/assets/button3.svg">
+                                            </div>
+                                        </div>
+                                        <div class="d-flex pb-sm-2 pl-sm-1 imagen" id="divImagen_${resImagen.id}">
+                                            <button class="align-self-end p-2">Ver mas...</button>
+                                        </div>
+                                    </div>`;
+                
+                divSugerencias.appendChild(sugerencia); 
+                const divImagen = document.getElementById(`divImagen_${resImagen.id}`);
+                divImagen.style.backgroundImage = `url(${resultado.data[index].images.downsized_large.url})`
+            }
+            return resultado;
+        })
+        .catch(error => {
+            return error;
+        });
+    return true;
+}
 
 function aplicarTemaDay(){
     console.log('day');
@@ -20,7 +61,7 @@ function aplicarTemaNight(){
 
 function mostrarOpciones(){
     const body = document.body;
-    body.classList.remove('body_light');
+    body.classList.remove('body_day');
     body.classList.add('body_night');
     /*const divOpciones = document.getElementsByClassName('opciones')[0];
     divOpciones.style.display = "block";*/
