1a10a613fa6e14ea800664893e55e624c422b999
Fernando Mociulsky <fmociulsky@garbarino.com.ar>
2020-04-30 13:26:10 -0300

    Sugerencias de busqueda

--- js/apps.js
+++ js/apps.js
@@ -2,11 +2,25 @@ const btnDay = document.getElementById('btn-day');
 const btnNight = document.getElementById('btn-night');
 const apiKey = 'ueK2jQMCymNZn085I319vMXEtEjvNFGH';
 
-
 agregarEventListeners();
 
 function agregarEventListeners(){
     document.addEventListener('DOMContentLoaded', onLoad());
+    document.addEventListener("click", (evt) => {
+        const flyoutElement = document.getElementById("div_buscador");
+        let targetElement = evt.target; // clicked element
+        
+        do {
+            if (targetElement == flyoutElement) {
+                mostrarSugBusquedas();
+                return;
+            }
+            targetElement = targetElement.parentNode;
+        } while (targetElement);
+        ocultarSugBusquedas();
+        return;
+    });
+    
 }
 
 function onLoad(){
@@ -16,17 +30,15 @@ function onLoad(){
 }
 
 function cargarSugerencias() {
-    const search = 'dog';
-    fetch('http://api.giphy.com/v1/gifs/search?q=' + search + '&api_key=' + apiKey)
+    fetch('http://api.giphy.com/v1/gifs/trending?api_key=' + apiKey + '&limit=4')
         .then(response => {
             return response.json();
         })
         .then(resultado => {
             console.log(resultado);
             const divSugerencias = document.getElementById('sugerencias');
-            for (let index = 0; index < 4; index++) {
-                const sugerencia = document.createElement('div');
-                const resImagen = resultado.data[index];
+            resultado.data.forEach(resImagen=>{
+                let sugerencia = document.createElement('div');
                 sugerencia.innerHTML = `<div class=" p-1 card-small"> 
                                         <div class="header p-1">
                                             <div class="d-flex justify-content-between">
@@ -41,9 +53,8 @@ function cargarSugerencias() {
                 
                 divSugerencias.appendChild(sugerencia); 
                 const divImagen = document.getElementById(`divImagen_${resImagen.id}`);
-                divImagen.style.backgroundImage = `url(${resultado.data[index].images.downsized_large.url})`
-            }
-            return resultado;
+                divImagen.style.backgroundImage = `url(${resImagen.images.fixed_height_small_still.url})`
+            });
         })
         .catch(error => {
             return error;
@@ -64,6 +75,7 @@ function aplicarTemaNight(){
     document.body.classList.add('body_night');
 
     document.getElementById("logo").src = "assets/gifOF_logo_dark.png";
+    ocultarOpciones();
 }
 
 function ocultarOpciones(){
@@ -76,15 +88,108 @@ function mostrarOpciones(){
     divOpciones.style.display = "block";
 }
 
+function buscarSugBusquedas(){
+    const valor = document.getElementById('buscar').value;
+    fetch('http://api.giphy.com/v1/tags/related/' + valor + '?api_key=' + apiKey + '&limit=3')
+    .then(response => {
+            return response.json();
+    }).then(resultado =>{
+        document.getElementById('busquedas_sug').style.display = "block";
+        const divBotones = document.getElementById('busquedas_sug_botones');
+        divBotones.innerHTML = "";
+        resultado.data.forEach(({name}) => {
+            const button = document.createElement('button');
+            button.classList = "py-md-2 my-2 btn-block text-left flex-column";
+            button.innerText = name;
+            divBotones.appendChild(button);
+        })
+    });
+}
+
 function cambiarEstiloBotonBuscar(){
     const botonBuscar = document.getElementById('boton_buscar');
     const inputBuscar = document.getElementById('buscar');
 
-    if(inputBuscar.value !== ''){
+    if(inputBuscar.value.trim()){
         botonBuscar.classList.remove('boton-inactivo');
         botonBuscar.classList.add('boton-activo');
+        mostrarSugBusquedas(inputBuscar.value.trim());
     }else{
         botonBuscar.classList.remove('boton-activo');
         botonBuscar.classList.add('boton-inactivo');
+        ocultarSugBusquedas();
     }
+}
+
+function buscar(){
+    const valor = document.getElementById('buscar').value;
+    if(valor != ""){
+        document.getElementById("titulo_resultado").innerText = valor;
+        const divResultados = document.getElementById('resultados');
+        divResultados.innerHTML = "";
+
+        fetch('http://api.giphy.com/v1/gifs/search?q=' + valor + '&api_key=' + apiKey)
+        .then(response => {
+                return response.json();
+            })
+        .then(resultado => {
+            if(resultado.data.length > 0){
+                let titulo = document.createElement('div');
+                titulo.innerHTML =    
+                resultado.data.forEach(resImagen=>{
+                    let res = document.createElement('div');
+                    res.innerHTML = `<div class="p-0 mb-md-3 card-result" onmouseover="resaltar(this, 'block')" onmouseout="resaltar(this, 'none')"> 
+                                            <div class="d-flex p-0 imagen" id="divImagen_${resImagen.id}">
+                                                <div class="p-1 flex-grow-1 p-0 mt-auto header">
+                                                    <div class="d-flex justify-content-between">
+                                                        <a href="#">#${resImagen.title.replace(/ /g, "").replace("GIF", "")}</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>`;
+                                        divResultados.appendChild(res); 
+                    const divImagen = document.getElementById(`divImagen_${resImagen.id}`);
+                    divImagen.style.backgroundImage = `url(${resImagen.images.fixed_height_small_still.url})`;
+                });
+            
+                const sec_sug = document.getElementById('sec_sugerencias');
+                if(sec_sug !=  null){
+                    const padre = sec_sug.parentNode;
+                    padre.removeChild(sec_sug);
+                }
+                document.getElementById('div_resultados').style.display = "block";
+            }else{
+                document.getElementById("titulo_resultado").style.display = "none";
+                const divResultados = document.getElementById('resultados');
+                divResultados.innerHTML = '<h2>No hay Resultados</h2>';
+            }
+            ocultarSugBusquedas();
+            return resultado;
+        })
+        .catch(error => {
+            console.log(error);
+            return error;
+        });
+    }else{
+        console.log("ingrese un valor");
+    }
+    
+}
+
+function mostrarSugBusquedas(valor){
+    buscarSugBusquedas(valor);
+}
+
+function ocultarSugBusquedas(){
+    document.getElementById('busquedas_sug').style.display = "none";
+}
+
+function resaltar(elemento, tipo){
+    elemento.getElementsByClassName("header")[0].style.display = tipo;
+    if(tipo == 'none'){
+        elemento.getElementsByClassName("imagen")[0].classList.remove("marco");
+    }else{
+        elemento.getElementsByClassName("imagen")[0].classList.add("marco");
+    }
+    
 }
